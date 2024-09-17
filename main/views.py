21d97a0aec1e5c4a362f1238a72b6e0f150dcbb4
anin02 <anindhya02@gmail.com>
2024-09-17 22:58:01 +0700

    Tugas 3

--- main/views.py
+++ main/views.py
@@ -1,20 +1,53 @@
-from django.shortcuts import render
-#from .models import Product  # Import model Product
+from django.shortcuts import render, redirect  # Tambahkan import redirect di baris ini
+from main.forms import ProductForm  # Import ProductForm untuk form produk
+from main.models import Product  # Import model Product
+from django.http import HttpResponse
+from django.core import serializers
+
+# Fungsi untuk membuat produk baru
+def create_product_entry(request):
+    # Inisialisasi form dengan data POST atau kosong
+    form = ProductForm(request.POST or None)
+
+    # Jika form valid dan metode request adalah POST
+    if form.is_valid() and request.method == "POST":
+        # Simpan data produk ke database
+        form.save()
+        # Redirect ke halaman utama setelah produk berhasil disimpan
+        return redirect('main:show_main')
+
+    # Buat konteks untuk template dengan form
+    context = {'form': form}
+    # Render halaman create_product_entry.html dengan form
+    return render(request, "create_product_entry.html", context)
 
 def show_main(request):
-    # Ambil semua produk dari database
-    #products = Product.objects.all()
+    # Mengambil semua produk dari database
+    product_entries = Product.objects.all()
 
-    # Buat konteks dengan data produk
+    # Membuat konteks untuk template
     context = {
-        "name": "Kacamata 1",
-        "price": 100,
-        "description": "Polarized",
-        "image_url": "https://www.charleskeith.co.id/dw/image/v2/BCWJ_PRD/on/demandware.static/-/Sites-id-products/default/dw246086b7/images/hi-res/2024-L3-CK3-71280563-34-1.jpg?sw=756&sh=1008",
-        "nama" : "Anindhyaputri Paramitha",
-        "kelas" : "PBP B",
-        "NPM" : "2306218111"
+        'name': 'Anindhyaputri Paramitha',  # Nama pemilik
+        'class': 'PBP B',  # Kelas
+        'npm': '2306218111',  # NPM
+        'product_entries': product_entries  # Semua produk yang diambil dari database
     }
 
-    # Render template dengan konteks
+    # Render template 'main.html' dengan konteks yang sudah dibuat
     return render(request, "main.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
