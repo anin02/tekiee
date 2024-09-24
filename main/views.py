d7a80b2f89293197c71e82d6f7ba32ee305013b0
anin02 <anindhya02@gmail.com>
2024-09-24 17:14:22 +0700

    coba tugas 4

--- main/views.py
+++ main/views.py
@@ -3,6 +3,15 @@ from main.forms import ProductForm  # Import ProductForm untuk form produk
 from main.models import Product  # Import model Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Fungsi untuk membuat produk baru
 def create_product_entry(request):
@@ -12,7 +21,9 @@ def create_product_entry(request):
     # Jika form valid dan metode request adalah POST
     if form.is_valid() and request.method == "POST":
         # Simpan data produk ke database
-        form.save()
+        product_entry = form.save(commit=False)  # Commit False untuk mengatur user
+        product_entry.user = request.user  # Menetapkan user yang membuat produk
+        product_entry.save()  # Simpan produk
         # Redirect ke halaman utama setelah produk berhasil disimpan
         return redirect('main:show_main')
 
@@ -21,16 +32,18 @@ def create_product_entry(request):
     # Render halaman create_product_entry.html dengan form
     return render(request, "create_product_entry.html", context)
 
+@login_required(login_url='/login')
 def show_main(request):
-    # Mengambil semua produk dari database
-    product_entries = Product.objects.all()
+    # Mengambil semua produk dari database yang ditambahkan oleh pengguna yang sedang login
+    product_entries = Product.objects.filter(user=request.user)
 
     # Membuat konteks untuk template
     context = {
         'name': 'Anindhyaputri Paramitha',  # Nama pemilik
         'class': 'PBP B',  # Kelas
         'npm': '2306218111',  # NPM
-        'product_entries': product_entries  # Semua produk yang diambil dari database
+        'product_entries': product_entries,  # Semua produk yang diambil dari database
+        'last_login': request.COOKIES['last_login'],
     }
 
     # Render template 'main.html' dengan konteks yang sudah dibuat
@@ -51,3 +64,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
--- tekie/settings.py
+++ tekie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-co@*4abg(s)j4y$5^@p#8=itt5*8wm((=uf%d)iw%)o)rt7!hz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "anindhyaputri-paramitha-tekiee.pbp.cs.ui.ac.id"]
 
